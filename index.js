ede673067008a3288160e629714c5e7689d257a8
aleadinglight <huutam.ptnk@gmail.com>
2018-07-07 02:12:40 +0700

    make some changes

--- index.js
+++ index.js
@@ -5,9 +5,28 @@ var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEv
 var recognition = new SpeechRecognition();
 var speechRecognitionList = new SpeechGrammarList();
 
+recognition.lang = 'en-US';
+recognition.interimResults = false;
+recognition.maxAlternatives = 1;
+
 var app = new Vue({
     el: '#app',
     data: {
         message:"Hello",
+    },
+    methods: {
+        startRecording: function (){
+            recognition.start();
+            this.message = "Recording";
+        }
     }
 });
+
+recognition.onresult = function(event) {
+    app.message = "received.";
+}
+
+recognition.onspeechend = function() {
+    app.message = "stop.";
+    recognition.stop();
+}
