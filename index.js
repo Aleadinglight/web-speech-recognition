2e63452c263b14acb5d957e038091a282c97affd
aleadinglight <huutam.ptnk@gmail.com>
2018-07-07 01:36:14 +0700

    added vue.js to index.html

---
+++ index.js
@@ -0,0 +1,13 @@
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
+var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList
+var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
+
+var recognition = new SpeechRecognition();
+var speechRecognitionList = new SpeechGrammarList();
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        message:"Hello",
+    }
+});
