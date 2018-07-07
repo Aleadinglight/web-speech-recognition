fa4e5e7a9523cf534d9b87bd65ff3c79761e8761
aleadinglight <huutam.ptnk@gmail.com>
2018-07-07 21:40:03 +0700

    added jekyll

---
+++ assets/js/index.js
@@ -0,0 +1,32 @@
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
+var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList
+var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
+
+var recognition = new SpeechRecognition();
+var speechRecognitionList = new SpeechGrammarList();
+
+recognition.lang = 'en-US';
+recognition.interimResults = false;
+recognition.maxAlternatives = 1;
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        message:"WELCOME",
+    },
+    methods: {
+        startRecording: function (){
+            recognition.start();
+            this.message = "RECORDING..";
+        }
+    }
+});
+
+recognition.onresult = function(event) {
+    app.message = event.results[0][0].transcript.toUpperCase();
+}
+
+recognition.onspeechend = function() {
+    app.message = "RECORDING STOPPED.";
+    recognition.stop();
+}
